5e4a7ac2c85cb68a8a309cf25a99d673e0648829
Kevin Tran <Kevin Tran>
2016-11-15 13:47:40 -0800

    aye lmao

--- exercise-1/scripts/buildMap.R
+++ exercise-1/scripts/buildMap.R
@@ -6,11 +6,22 @@ library(plotly)
 # BuildMap function: fill this in with a function that returns a map:
 # Try parameterize a few options, such as the title
 # I suggest: https://plot.ly/r/bubble-maps/
-BuildMap <- function(data) {
-  
-  
-  
 
+BuildMap <- function(data) {
+  g <- list(
+    scope = 'usa',
+    projection = list(type = 'albers usa'),
+    showland = TRUE,
+    landcolor = toRGB("gray85"),
+    subunitwidth = 1,
+    countrywidth = 1
+  )
   
-  
+  p <- plot_geo(data, locationmode = 'USA-states') %>%
+    add_markers(
+      x = ~lon, y = ~lat, hoverinfo = 'text',
+      text = ~paste(name, "<br />Population: ", pop)
+    ) %>%
+    layout(title = '2016 US Shooting Casualties by City / Affected Casualties', geo = g)
+  return(p)
 }
--- exercise-2/ui.R
+++ exercise-2/ui.R
@@ -4,7 +4,24 @@
 shinyUI(fluidPage(
   
   # Add a numbericInput to choose the number of observations
-  
+  titlePanel("Hello Shiny!"),
+  numericInput(label="# observations", value = 100, min = 1, step = 1), 
+  selectInput('color', label =  'color', default), 
+  # Sidebar with a slider input for the number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
   
   # Add a selectInput that allows you to select a color from a list of choices
   
